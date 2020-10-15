c35753fa58ba8561f11862c6587f8a70c7399ebc
Godwin Obamina <38545029+Owebiwan@users.noreply.github.com>
2020-10-15 12:54:10 +0100

    Updates

---
+++ precache-manifest.8a9f6aa3a96457f34f5b4582f1a3a317.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7a21f952d1668c59fad0cc18e1863d",
+    "url": "/github.io/portfolio/index.html"
+  },
+  {
+    "revision": "386c744f519a1119113d",
+    "url": "/github.io/portfolio/static/css/main.872c669f.chunk.css"
+  },
+  {
+    "revision": "9e51589edd8a2d18f91e",
+    "url": "/github.io/portfolio/static/js/2.266aa5a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/portfolio/static/js/2.266aa5a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386c744f519a1119113d",
+    "url": "/github.io/portfolio/static/js/main.0edcabb5.chunk.js"
+  },
+  {
+    "revision": "2c269339647890a550ed",
+    "url": "/github.io/portfolio/static/js/runtime-main.2fc02a7e.js"
+  },
+  {
+    "revision": "5376e6b5dba361cf76c664c5bfbeaf1e",
+    "url": "/github.io/portfolio/static/media/bk1.5376e6b5.png"
+  },
+  {
+    "revision": "5542daf0de9af85454a9e20bd3b008a8",
+    "url": "/github.io/portfolio/static/media/intro.5542daf0.png"
+  },
+  {
+    "revision": "44c26c68f40e2bb8c975598d99e3faab",
+    "url": "/github.io/portfolio/static/media/legate.44c26c68.png"
+  },
+  {
+    "revision": "05afd1df54a26f05c50e6c2c5f34351b",
+    "url": "/github.io/portfolio/static/media/logo.05afd1df.png"
+  },
+  {
+    "revision": "482200f3d841d7762a009f425467370f",
+    "url": "/github.io/portfolio/static/media/markup.482200f3.png"
+  },
+  {
+    "revision": "a7f5fcb67357955233721435e539b866",
+    "url": "/github.io/portfolio/static/media/petsclique.a7f5fcb6.png"
+  },
+  {
+    "revision": "9076e861699bc383bfbaa541656daae0",
+    "url": "/github.io/portfolio/static/media/profilepic.9076e861.png"
+  }
+]);
